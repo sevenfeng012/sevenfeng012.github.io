69b9efd37ea65dfa8ae39146539d881fb82a9341
sevenfeng012 <fengzhijian012@163.com>
2018-11-05 13:24:17 +0800

    Site updated: 2018-11-05 13:24:16

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Nov 03 2018 15:36:53 GMT+0800 (CST)
+// build time:Mon Nov 05 2018 13:24:07 GMT+0800 (CST)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
